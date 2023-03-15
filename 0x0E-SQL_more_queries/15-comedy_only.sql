1fe5fa39f558e7e40c579befcf793f0a9e18c50e
abiolaolaide <ayodeleabiola1994@gmail.com>
2023-03-15 10:19:38 -0700

    task correction

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,11 @@
--- a script lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
